72067c6720eddbc490cfa088d954403630793e5f
afew3232 <60533831+afew3232@users.noreply.github.com>
2020-03-17 16:34:15 +0900

    delete byebug  from application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,24 +1,18 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
+	before_action :configure_permitted_parameters, if: :devise_controller?
 	def after_sign_in_path_for(resource)
-		byebug
 		case resource
 		when Admin
 			admin_root_path
 		when User
 			root_path
 		end
-
 	end
 
-  def after_sign_up_path_for(resource)
-    root_path
-  end
-
-  protected
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:firstname, :lastname, :firstname_kana, :lastname_kana, :email, :password, :postal, :address, :telephone])
-      devise_parameter_sanitizer.permit(:sign_in, keys: [:name, :password])
-    end
+	protected
+	def configure_permitted_parameters
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:firstname, :lastname, :firstname_kana, :lastname_kana, :email, :password, :postal, :address, :telephone])
+		devise_parameter_sanitizer.permit(:sign_in, keys: [:name, :password])
+	end
 end
 
