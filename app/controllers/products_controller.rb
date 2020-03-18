1a11612e4775b07fe8c79937e84fa1101d0fbd48
yuna1001 <hegapoo1001@gmail.com>
2020-03-18 00:43:36 +0000

    [Add] 商品一覧ページを追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,18 @@
 class ProductsController < ApplicationController
   def index
+    if params[:genres_id]
+    # genres_idと紐づく商品を取得
+    @products = Product.where(:genre_id).all
+    else
+    # 商品をすべてを取得
+    @products = Product.all
+    end
+    @genres = Genre.all
   end
 
   def show
   end
+
 end
+
+
