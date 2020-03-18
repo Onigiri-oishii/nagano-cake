1978aedefb045afc7150199f35d756ded2d3061b
afew3232 <60533831+afew3232@users.noreply.github.com>
2020-03-18 17:38:03 +0900

    [Add]管理者画面　ユーザー編集画面

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -6,8 +6,21 @@ class Admin::UsersController < ApplicationController
   end
 
   def edit
+  	@user = User.find(params[:id])
   end
 
   def update
+  	@user = User.find(params[:id])
+  	if @user.update(user_params)
+  		redirect_to admin_user_path(params[:id])
+  	else
+  		render "admin/users/edit"
+  	end
+  end
+
+
+  private
+  def user_params
+  	params.require(:user).permit(:lastname, :ifirstname, :lastname_kana, :firstname_kana, :email, :postal, :address, :telephone, :password, :status)
   end
 end
