143e6c82d8fbeb16bb5db217fd0904efbfd1cab4
shogo0625 <shogo19940625@gmail.com>
2020-03-18 09:54:01 +0000

    [Add] 配達先登録/一覧ページ＆配達先編集ページ作成

--- app/controllers/receivers_controller.rb
+++ app/controllers/receivers_controller.rb
@@ -1,16 +1,44 @@
 class ReceiversController < ApplicationController
   def index
+    @user = User.find(params[:user_id])
+    @receiver = current_user.receivers.new
+    @receivers = current_user.receivers.all
   end
 
   def create
+    @receiver = current_user.receivers.new(receiver_params)
+    if @receiver.save
+      redirect_to user_receivers_path
+    else
+      @user = User.find(params[:user_id])
+      @receivers = current_user.receivers.all
+      render 'index'
+    end
   end
 
   def destroy
+    @receiver = current_user.receivers.find(params[:id])
+    @receiver.destroy
+    redirect_to user_receivers_path
   end
 
   def edit
+    @user = User.find(params[:user_id])
+    @receiver = current_user.receivers.find(params[:id])
   end
 
   def update
+    @receiver = current_user.receivers.find(params[:id])
+    if @receiver.update(receiver_params)
+      redirect_to user_receivers_path
+    else
+      @user = User.find(params[:user_id])
+      render 'edit'
+    end
+  end
+
+  private
+  def receiver_params
+    params.require(:receiver).permit(:postal, :address, :name)
   end
 end
