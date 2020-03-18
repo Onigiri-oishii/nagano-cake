bf0d785a0a9b266c8d7fe0e18c8e8449fde8dd90
yuna1001 <hegapoo1001@gmail.com>
2020-03-18 12:32:42 +0000

    [Fix] 商品一覧ページの修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,20 +1,23 @@
 class ProductsController < ApplicationController
   def index
-    if params[:genres_id]
-    # genres_idと紐づく商品を取得
-    @products = Product.where(:genre_id).all
-    else
-    # 商品をすべてを取得
-    @products = Product.all
-    end
     @genres = Genre.all
+    if params[:search]
+      @products = Product.where(products_search_params).page(params[:page]).per(6)
+    else
+      @products = Product.all.page(params[:page]).per(6)
+    end
   end
 
   def show
-  	@genres = Genres.all
-  	@product = Product.find(params[:id])
-  	@user = current_user
-  	@cart_product = @user.cart_product.new
+    @genres = Genre.all
+    @product = Product.find(params[:id])
+    @user = current_user
+    @cart_product = @user.cart_products.new
+  end
+
+  private
+  def products_search_params
+    params.fetch(:search, {}).permit(:genres_id)
   end
 
 end
