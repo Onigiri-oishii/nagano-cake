acc213a434eb696318ce9eec7c22e9dc45e8badf
rist-9 <yasu.sss9@gmail.com>
2020-03-19 05:17:14 +0000

    finish genre edit view

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,19 +1,34 @@
 class Admin::GenresController < ApplicationController
   def index
+    @genre = Genre.new
+    @genres = Genre.all
   end
 
   def create
+    if @genre = Genre.create(params_genre)
+      redirect_to admin_genres_path
+    else
+      @genres = Genre.all
+      render "admin/genres/index"
+    end
   end
 
   def edit
+    @genre = Genre.find(params[:id])
   end
 
   def update
-  	　@genre = Genre.find(params[:id])
+      @genre = Genre.find(params[:id])
+    if @genre.update(params_genre)
+      redirect_to admin_genres_path
+    else
+      render "admin/genres/edit"
+    end
   end
 
   private
-      def genre_params
-    　params.require(:genre).permit(:ststus)
-      end
+  def params_genre
+    params.require(:genre).permit(:name, :status)
+  end
+
 end
