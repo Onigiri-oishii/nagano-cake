864afcfb7aad1a80f26dd087b3b8d3d856bf9f45
afew3232 <60533831+afew3232@users.noreply.github.com>
2020-03-19 11:24:49 +0900

    [Add]管理者 ジャンル一覧・登録画面

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,8 +1,15 @@
 class Admin::GenresController < ApplicationController
   def index
+  	@genre = Genre.new
+  	@genres = Genre.all
   end
 
   def create
+  	if @genre = Genre.create(params_genre)
+  		redirect_to admin_genres_path
+  	else
+  		render "admin/genres/index"
+  	end
   end
 
   def edit
@@ -10,4 +17,10 @@ class Admin::GenresController < ApplicationController
 
   def update
   end
+
+  private
+  def params_genre
+  	params.require(:genre).permit(:name, :status)
+  end
+
 end
