39f5d00713a21514b6562d8155906750866be8a2
rist-9 <yasu.sss9@gmail.com>
2020-03-21 03:43:34 +0000

    Finish-genre-edit

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -14,9 +14,16 @@ class Admin::GenresController < ApplicationController
   end
 
   def edit
+    @genre = Genre.find(params[:id])
   end
 
   def update
+    @genre = Genre.find(params[:id])
+    if @genre.update(params_genre)
+      redirect_to admin_genres_path
+    else
+      render "admin/genres/edit"
+    end
   end
 
   private
