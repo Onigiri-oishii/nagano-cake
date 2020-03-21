6b585470a61bd3501d387af0a48f88938dc1134e
shogo0625 <shogo19940625@gmail.com>
2020-03-21 14:20:32 +0000

    [Add] 会員 カート内商品一覧ページ作成

--- app/controllers/cart_products_controller.rb
+++ app/controllers/cart_products_controller.rb
@@ -1,14 +1,28 @@
 class CartProductsController < ApplicationController
   def index
+    @user = current_user
+    @cart_products = current_user.cart_products.all
   end
 
   def update
+    @cart_product = current_user.cart_products.find(params[:id])
+    if @cart_product.update(cart_product_params)
+      redirect_to user_cart_products_path
+    else
+      render :index
+    end
   end
 
   def destroy
+    @cart_product = current_user.cart_products.find(params[:id])
+    @cart_product.destroy
+    redirect_to user_cart_products_path
   end
 
   def destroy_all
+    @cart_products = current_user.cart_products.all
+    @cart_products.destroy
+    redirect_to user_cart_products_path
   end
 
   def create
