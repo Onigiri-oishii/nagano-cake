8b2fbd2b83dfc7733071ab1de0421138bbf65530
shogo0625 <shogo19940625@gmail.com>
2020-03-23 02:21:28 +0000

    [Add] 会員注文確認画面作成

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,6 +4,7 @@ class Order < ApplicationRecord
 
 	has_many :order_products, dependent: :destroy
 	accepts_nested_attributes_for :order_products
+
 	belongs_to :user
 
 	validates :address, presence: true
