3bfcc2cd02fea42b9605c0418c75344f7171132b
afew3232 <60533831+afew3232@users.noreply.github.com>
2020-03-26 16:39:57 +0900

    [Fix] user-product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,24 +7,7 @@ class ProductsController < ApplicationController
       @products = Product.where(products_search_params).page(params[:page]).per(6)
       @genreid = params[:genres_id_var]
     else
-      #----------Product内でgenreが有効なもの　かつ　ページネーション----------
-      # byebug
-      # @propro = Genres.products.where(status: true)
-      # productbuf = Product.all
-      # product_buf_search = Product.new
-      # productbuf.each do |p|
-      #   if Genre.find(p.genres_id).status == true
-      #     product_buf_search = product_buf_search + p
-      #     product_buf_search.where
-      #   end
-      # end
-      #@products = Product.where(.status == true).page(params[:page]).per(6)
-
-
-     # @pro = Product.where(status: true).page(params[:page]).per(6)
-
       @products = Product.where(genre_status: true).page(params[:page]).per(6)
-      byebug
     end
   end
 
